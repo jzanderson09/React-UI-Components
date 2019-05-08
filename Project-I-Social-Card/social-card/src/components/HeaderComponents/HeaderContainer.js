09a624a6fbecc75f361b0a890b98f5f660db50b0
jzanderson09 <jzanderson09@gmail.com>
2019-05-08 00:50:14 -0600

    App.js imports from <HeaderContainer />. Set up <HeaderContainer /> with imports from <ImageThumbnail />, <HeaderTitle /> and <HeaderContent /> --> each import has its respective JSX established.  Header.css updated with styling and notes for each .js section. Will continue with <cardContainer /> next.

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Header.css';
+
+import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+
+const HeaderContainer = () => {
+    return (
+        <header className="header-container">
+            <ImageThumbnail />
+            <HeaderTitle />
+            <HeaderContent />
+        </header>
+    );
+};
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,24 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    let currentDate = new Date();
+    let date = currentDate.getDate();
+    let month = 'May';
+    let dateString = date.toString() + ' ' + month;
+    return(
+        <div className="header-title">
+            <h3 className="title-items title">Lambda School</h3>
+            <h3 className="title-items twitter-link">
+                <a href="https://twitter.com/lambdaschool?lang=en">@lambdaSchool</a>
+            </h3>
+            {/* <h3 className="title-items period">.</h3> */}
+            <h3 className="title-items date">{dateString}</h3>
+        </div>
+
+
+
+    );
+};
+
+export default HeaderTitle;
